bd002e00d9465327b9880db4208c61b47d4ece6f
Iyán Méndez Veiga <iyan.mendezveiga@hslu.ch>
2022-09-20 00:11:27 +0200

    Initial commit

---
+++ arxivbot.py
@@ -0,0 +1,48 @@
+import re
+import requests
+import datetime
+from bs4 import BeautifulSoup
+from maubot import Plugin
+from maubot.handlers import command
+
+
+class ArXivBot(Plugin):
+
+    arxiv_regex = re.compile(r'(https?://arxiv\.org/abs/(?:quant-ph/)?[0-9\.]*)')
+
+    def _parse_arxiv(self, url):
+        r = requests.get(url)
+        xml = r.text
+        soup = BeautifulSoup(xml, features="xml")
+        authors = []
+        for elem in soup.head.find_all("meta"):
+            try:
+                name = elem["name"]
+            except KeyError:
+                pass
+            if name == "citation_title":
+                title = elem["content"]
+            elif name == "citation_author":
+                author = elem["content"].split(", ")
+                authors.append(" ".join([author[-1], author[0]]))
+            elif name == "citation_date":
+                date = datetime.datetime.strptime(elem["content"], "%Y/%m/%d")
+                date = date.strftime("%d.%m.%Y")
+            elif name == "citation_pdf_url":
+                pdf = elem["content"]
+            elif name == "citation_abstract":
+                abstract = elem["content"].strip()
+        authors = ", ".join(authors)
+        return {"title": title, "authors": authors, "date": date, "abstract": abstract, "pdf": pdf}
+
+    def _output_text(self, d):
+        return f'**Date**: {d["date"]}\n\n**Title**: {d["title"]}\n\n**Authors**: {d["authors"]}\n\n**Abstract**: {d["abstract"]}\n\n**PDF**: {d["pdf"]}'
+
+    @command.passive(arxiv_regex)
+    async def arxiv(self, evt, matches):
+        # Remove duplicates
+        matches = [*set(matches)]
+        for match in matches:
+            d = self._parse_arxiv(match)
+            out = self._output_text(d)
+            await evt.reply(out, markdown=True)
